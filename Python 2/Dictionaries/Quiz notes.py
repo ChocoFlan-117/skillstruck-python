66ba9da8a59949e5c063f3fa935cf08cc53216ab
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:27 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries/Quiz notes.py
@@ -0,0 +1,46 @@
+######
+#Dictionaries are...
+#Changeable, ondexed, ordered
+######
+
+
+######
+#The first item in the pairing of a dictionary is called the...
+#key
+######
+
+
+#######
+#The second item in a dictionary pairing is called the...
+#value
+#######
+
+
+#######
+#What is the biggest difference between dictionaries and lists in Python?
+#Dictionaries have pairings while lists don't.
+#######
+
+
+#######
+#Which data structure is ordered, changeable, and indexed in Python?
+#Dictionaries
+########
+
+
+#######
+#How is data stored in a Python dictionary?
+#As a pair of key-value items
+########
+
+
+########
+#What is the purpose of curly braces ({}) in a dictionary?
+#To enclose a dictionary
+########
+
+
+#######
+#How do you access the value associated with the key "Vance" in the classmates dictionary?
+#classmates["Vance"]
+########

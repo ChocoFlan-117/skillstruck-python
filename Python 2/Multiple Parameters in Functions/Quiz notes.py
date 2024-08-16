8c0de8954126c7e1c35f46951c7272ece855e39d
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:20 -0700

    exported from Skill Struck

---
+++ Python 2/Multiple Parameters in Functions/Quiz notes.py
@@ -0,0 +1,52 @@
+########
+#True or False: Python functions cannot accept multiple parameters.
+#False
+########
+
+########
+#True or False: You can have 3 or more parameters in a Python function.
+#True
+########
+
+########
+#What will the following code print out?
+def breads(first, second):
+	print("I will have to go buy some " + first + " and " + second)
+
+
+breads("rolls", "baguettes")
+#I will have to go buy some rolls and baguettes
+#######
+
+#######
+#What will the following code print out?
+def breads(first, second):
+	print("I will have to go buy some " + first + " and " + "second")
+
+
+breads("rolls", "baguettes")
+#I will have to go buy some rolls and second
+#######
+
+#######
+#What is a variable called if it's created inside a function?
+#local variable
+#######
+
+#######
+#In the code example, what kind of variable is the variable named class?
+class = "I have chemistry class" 
+def myfunction():
+    homework = "essay"
+    print(homework)
+
+myfunction()
+
+print(favorite)
+#global variable
+#######
+
+#######
+#What are variables called if they are created outside the function?
+#Global variable
+#######

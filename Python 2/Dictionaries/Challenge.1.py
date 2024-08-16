5998aae31719d70362f2b85e0b9255e702db2389
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:27 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries/Challenge.1.py
@@ -0,0 +1,16 @@
+#Dictionary Author
+
+dictionary = {
+    "m" : 1,
+    "e" : 2,
+    "o" : 3,
+    "w" : 4,
+    "c" : 5,
+    "a" : 6,
+    "t" : 7,
+    "s" : 8,
+    "wo" : 9,
+    "oo" : 10,
+}
+
+print(dictionary)

babfd931b90618e68742d4c1d456e398a997a845
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:25 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries/Code Checkpoint.py
@@ -0,0 +1,10 @@
+mountains = {
+    "Timpanogos" : 1,
+    "Everest" : 2,
+    "Kilimanjaro" : 3,
+    "Vesuvius" : 4,
+}
+
+print(mountains)
+print(mountains["Vesuvius"])
+mountains["Kilimanjaro"] = 10

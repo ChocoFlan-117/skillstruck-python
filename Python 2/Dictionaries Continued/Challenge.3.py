cb5e0a248e6790a6a9e2efb1b6b638f67910ac1f
Gato-117 <bubal616@evit.edu>
2024-08-16 13:24:48 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries Continued/Challenge.3.py
@@ -0,0 +1,13 @@
+#Word counter#
+
+#unsure how to continue
+#note to self: return to this later
+word = [str(n) for n in input().split()]
+times_seen = {}
+
+for  in :
+	if  not in :
+		times_seen[word] = 
+	else:
+		times_seen[word] += 
+	print([])

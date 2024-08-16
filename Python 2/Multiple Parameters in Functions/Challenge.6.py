32387c12b089c28120808a0ffa9b4e5bf1e2cf02
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:24 -0700

    exported from Skill Struck

---
+++ Python 2/Multiple Parameters in Functions/Challenge.6.py
@@ -0,0 +1,47 @@
+#What day is it tomorrow function
+
+month = int(input("Month: "))
+date = int(input("Date: "))
+
+def tomorrow(month, date):
+    if month == 1 and date <= 31:
+        ndate = date + 1
+        if date == 31:
+            print("new month" + ndate)
+        else:
+            print("same month" + ndate)
+
+   if month == 2 and date <= 31:
+        ndate = date + 1
+        if date == 31:
+            print("new month" + ndate)
+        else:
+            print("same month" + ndate)
+
+    if month == 3 and date <= 31:
+        ndate = date + 1
+        if date == 31:
+            print("new month" + ndate)
+        else:
+            print("same month" + ndate)
+
+    if month == 4 and date <= 31:
+        ndate = date + 1
+        if date == 31:
+            print("new month" + ndate)
+        else:
+            print("same month" + ndate)
+
+    if month == 5 and date <= 31:
+        ndate = date + 1
+        if date == 31:
+            print("new month" + ndate)
+        else:
+            print("same month" + ndate)
+
+    if month == 6 and date <= 31:
+        ndate = date + 1
+        if date == 31:
+            print("new month" + ndate)
+        else:
+            print("same month" + ndate)

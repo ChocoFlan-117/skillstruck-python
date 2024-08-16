b077035ece66ca4dcf89e734532b78a521f5b233
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:21 -0700

    exported from Skill Struck

---
+++ Python 2/Multiple Parameters in Functions/Challenge.1.py
@@ -0,0 +1,10 @@
+#Hungry for apples 3
+
+fruit1 = input("Please enter the name of the fruit you would like: ")
+fruit2 = input("Please enter the name of the fruit you would like: ")
+fruit3 = input("Please enter the name of the fruit you would like: ")
+
+def yum(fruit1, fruit2, fruit3):
+    print(fruit1 + fruit2 + fruit3)
+
+yum(fruit1, fruit2, fruit3)

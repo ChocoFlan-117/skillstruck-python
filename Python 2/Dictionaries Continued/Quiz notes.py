d5a03481eda3b26b59a9ba7bfdb2d5439b11c7cc
Gato-117 <bubal616@evit.edu>
2024-08-16 13:24:46 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries Continued/Quiz notes.py
@@ -0,0 +1,44 @@
+#######
+#What will this code do? books.pop("Odyssey")
+#It will take "Odyssey" out of the dictionary named books
+########
+
+########
+#What will the following code do? print(len(flavors))
+#It will print out the length of the dictionary named flavors.
+########
+
+#########
+#What will the following code print out?
+classmates = {
+    "Billy": 8,
+    "Vance": 15,
+    "Alice": 10,
+    "Lily": 6,
+    "Xavier": 12
+}
+print(len(classmates))
+#5
+##########
+
+###########
+#What does the following code do?
+if "Vance" in classmates:
+    print("Vance is in your dictionary")
+#Prints "Vance is in your dictionary" if Vance is in the dictionary
+############
+
+############
+#How can you check if "Shelby" is not in the dictionary?
+#if "Shelby" not in classmates:
+############
+
+############
+#The length of a dictionary is determined by:
+#The number of key/value pairs are in the dictionary
+############
+
+############
+#When counting the length of a dictionary, where do we start counting from?
+#1
+#############

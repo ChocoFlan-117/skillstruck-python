fddb818bb5dacd3868ac13acda02930b88ae27fb
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:18 -0700

    exported from Skill Struck

---
+++ Notes/Multiple parameters in functions.4.py
@@ -0,0 +1,15 @@
+ #Notes:
+
+#Functions can accept more than one parameter
+
+#Example:
+def gifts(first, second):
+    print("Your first choice for a birthday gift would be " +  first)
+    print("Your second choice for a birthday gift would be " + second)
+
+gifts("bike", "basketball")
+gifts("speaker", "tickets")
+#Your first choice for a birthday gift would be bike
+#Your second choice for a birthday gift would be basketball
+#Your first choice for a birthday gift would be speaker
+#Your second choice for a birthday gift would be tickets

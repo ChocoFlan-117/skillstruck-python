3c10d491958ef5ea3175aa5e89b56582c0d8cc8c
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:29 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries/Challenge.4.py
@@ -0,0 +1,17 @@
+#Color Dictionary
+
+colors = {
+    "red" : 1,
+    "blue" : 2,
+    "orange" : 3,
+    "purple" : 4,
+    "pink" :5,
+}
+
+print(colors["orange"])
+print(colors["purple"])
+
+colors["red"] = 12
+colors["blue"] = 23
+
+print(colors)

4a97296c9b153bae8d120881b20f432e9c366c8a
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:32 -0700

    exported from Skill Struck

---
+++ Python 2/Adding to and removing from dictionaries/Quiz notes.py
@@ -0,0 +1,34 @@
+#Which method can be used to remove an item from a dictionary?
+#pop()
+############
+
+############
+#True or False: It is possible to add and remove items from a Python dictionary
+#True
+###########
+
+###########
+#How can you add new items to a dictionary?
+#Using square brackets and the key assignment
+###########
+
+###########
+#What will be the output of the following code?
+classmates = {"Billy": 8, "Vance": 15, "Alice": 10}
+classmates["Lily"] = 6
+print(classmates)
+#{'Billy': 8, 'Vance': 15, 'Alice': 10, 'Lily': 6}
+###########
+
+###########
+#How do you remove an item from a dictionary using the "pop" method?
+#classmates.pop("Alice")
+###########
+
+###########
+#What will be the output of the following code?
+classmates = {"Billy": 8, "Vance": 15, "Alice": 10, "Lily": 6, "Xavier": 12}
+classmates.pop("Alice")
+print(classmates)
+#{'Billy': 8, 'Vance': 15, 'Lily': 6, 'Xavier': 12}
+###########

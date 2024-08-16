dfdff19839f7813e72e7590ecf37eab4496b5b91
Gato-117 <bubal616@evit.edu>
2024-08-16 13:24:47 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries Continued/Challenge.1.py
@@ -0,0 +1,12 @@
+#How many?
+item1 = input("Add to dictionary: ")
+item2 = input("Add to dictionary: ")
+item3 = input("Add to dictionary: ")
+
+dictionary = { 1: "bicycle", 2: "soccer balls", 3: "piano books", 4: "insert", 5: "insert", 6: "insert" }
+
+dictionary[4] = item1
+dictionary[5] = item2
+dictionary[6] = item3
+
+print(dictionary)

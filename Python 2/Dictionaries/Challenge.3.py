39528888e79c87ff010d2bdff4fc1053ca2209a3
Gato-117 <bubal616@evit.edu>
2024-08-16 12:15:29 -0700

    exported from Skill Struck

---
+++ Python 2/Dictionaries/Challenge.3.py
@@ -0,0 +1,18 @@
+#Survival
+
+knife = int(input("Knife rating: "))
+fire = int(input("Fire starter rating: "))
+pot = int(input("Pot rating: "))
+rope = int(input("Rope rating: "))
+tarp = int(input("Tarp rating: "))
+
+rating = {
+    "knife" : knife,
+    "fire starter" : fire,
+    "pot" : pot,
+    "rope" : rope,
+    "tarp" : tarp,
+}
+
+
+print(rating)

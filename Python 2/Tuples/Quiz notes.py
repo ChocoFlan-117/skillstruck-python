a96f9646ada6054bb5662d8392a9897d46a7373d
Gato-117 <bubal616@evit.edu>
2024-08-21 13:02:10 -0700

    exported from Skill Struck

---
+++ Python 2/Tuples/Quiz notes.py
@@ -0,0 +1,55 @@
+#What can be done with a tuple? Select all that apply:
+#Items can be accessed from inside a tuple.
+###################
+
+
+################
+#What are ways that Python groups information together? Select all that apply:
+#Lists, Tuples, Dictionaries
+
+
+##############
+#What will print from this code?
+desserts = ("candy", "popcorn", "soda", "cake")
+print(desserts[-1])
+#"cake"
+###########
+
+
+##########
+#What is the main difference between tuples and lists?
+#Lists are changeable, tuples are not
+##############
+
+
+##############
+#True or False: Tuples can be changed
+#False
+############
+
+
+##############
+#How can you access the second item in a tuple named 'instruments'?
+instruments[1]
+#################
+
+
+################
+#What is the result of the following code:
+instruments = ("clarinet", "piano", "drum", "violin", "guitar") 
+print(instruments[1:3])
+('piano', 'drum')
+#############
+
+
+############
+#How do you check if the value "piano" exists in the tuple 'instruments'?
+#"piano" in instruments
+#################
+
+
+################
+#What is the result of the following code:
+instruments = ("clarinet", "piano", "drum", "violin") print(instruments[-2])
+#drum
+################

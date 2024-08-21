282d0ee404d96f047e4a19e93781ebcfa6d1bd6a
Gato-117 <bubal616@evit.edu>
2024-08-21 13:01:58 -0700

    exported from Skill Struck

---
+++ Python 2/Looping through a dictionary /Quiz notes.py
@@ -0,0 +1,65 @@
+What will the following code print out?
+classmates = {
+    "Billy" : 8,
+    "Vance" : 12,
+    "Alice" : 10,
+    "Eliza" : 15,
+    "Xavier" : 6,
+}
+
+for x in classmates:
+    print(x)
+
+The keys
+
+
+
+True or False: It is possible to print out both the keys and the values when looping through a dictionary.
+True
+
+
+
+What will the following code print out?
+animals = {"sheep": 8, "cows": 12, "chickens": 10}
+
+for x in animals:
+	print(x)
+
+sheep cows chickens
+
+
+
+In the following example, the term "octopus" is considered a what?
+fish = {"squid": 12, "octopus": 2, "shark": 8}
+
+for x in fish:
+	print(x)
+
+a key
+
+
+
+What will be printed when the following code is executed?
+classmates = {"Billy": 8, "Vance": 12, "Alice": 10, "Eliza": 15, "Xavier": 6}
+for x in classmates.values():
+    print(x)
+
+8 12 10 15 6
+
+
+
+How can you access the values in the "classmates" dictionary using a loop?
+for x in classmates.values():
+
+
+
+What will the following code print out from a dictionary named classmates?
+for x, y in classmates.items():
+    print(x, y)
+
+Print the items (keys and values) in the dictionary
+
+
+
+True or False: Looping through a dictionary is similar to looping through a list.
+True

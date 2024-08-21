967b116f588b94d7c315453eeab31806ea34ff1c
Gato-117 <bubal616@evit.edu>
2024-08-21 13:02:06 -0700

    exported from Skill Struck

---
+++ Python 2/Looping through a dictionary continued/Challenge.3.py
@@ -0,0 +1,12 @@
+#Power Dictionary#
+
+#I don't understand how to do this
+
+keys = int(input("How many keys are in your dictionary?" ))
+
+dictionary = {}
+
+for i in range(keys):
+    
+        
+print(dictionary)
